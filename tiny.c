4662034d21e8196d81b11a2445024806151c2a62
Thomas Brand <tom@trellis.ch>
2020-10-06 20:20:50 +0200

    argv[1] -> path

--- tiny.c
+++ tiny.c
@@ -425,16 +425,16 @@ int main(int argc, char** argv) {
 			default_port = atoi(argv[1]);
 		} else {
 			path = argv[1];
-			if(chdir(argv[1]) != 0) {
-				perror(argv[1]);
+			if(chdir(path) != 0) {
+				perror(path);
 				exit(1);
 			}
 		}
 	} else if (argc == 3) {
 		default_port = atoi(argv[2]);
 		path = argv[1];
-		if(chdir(argv[1]) != 0) {
-			perror(argv[1]);
+		if(chdir(path) != 0) {
+			perror(path);
 			exit(1);
 		}
 	}
